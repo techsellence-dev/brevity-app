4860e8749afbcd71b843c3526058ea7582d83316
abhijan2402 <abhishek.jangid643@gmail.com>
2022-07-21 11:05:53 +0530

    notification changes

--- src/home/frontend/components/menu/BasicMenu.js
+++ src/home/frontend/components/menu/BasicMenu.js
@@ -1,30 +1,142 @@
-import React from "react";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Box from "@mui/material/Box";
-import { blue } from "@mui/material/colors";
+// import React, { useEffect, useState } from "react";
+// import Menu from "@mui/material/Menu";
+// import MenuItem from "@mui/material/MenuItem";
+// import Box from "@mui/material/Box";
+// import { blue } from "@mui/material/colors";
+// import { listNotifications } from '../../../../gqlFunctions/NotifTable'
+// import { API } from 'aws-amplify';
+// import * as mutations from '../../../../graphql/mutations';
+// import * as queries from '../../../../graphql/queries';
+// //change colour of menu item based on seen not seen
 
-//change colour of menu item based on seen not seen
+// const BasicMenu = ({ anchore, handleClose, open, menuItems }) => {
+//   const [status, setStatus] = useState("Unseen")
+//   const [bgcolor, setBgcolor] = useState("#69a832")
+//   const notifications = [
+//     {
+//       id: 0,
+//       label: "First notification",
+//       status: { status }
+//     },
+//     {
+//       id: 1,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 2,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 3,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 4,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 5,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 6,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 7,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 8,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 9,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 10,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 11,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 12,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 13,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 14,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//     {
+//       id: 15,
+//       label: "Second notification",
+//       status: { status }
+//     },
+//   ];
+//   const [listnf, setListnf] = useState([]);
+//   useEffect(() => {
+//     const listNotifications = async () => {
+//       try {
+//         const listNotifData = await API.graphql({ query: queries.listUserNotifications });
+//         console.log(listNotifData.data.listUserNotifications.items);
+//         setListnf(listNotifData.data.listUserNotifications.items)
+//         console.log(listnf)
+//       } catch (error) {
+//         console.log("Error in listing", error)
+//         throw new Error(error)
+//       }
+//     }
+//     listNotifications()
+//   }, [])
+//   function clicky() {
+//     console.log("yes")
+//     setBgcolor("white")
+//     setStatus("seen")
+//   }
+//   console.log(`anchor in basic menu is ${anchore}`);
+//   return (
+//     <Menu
+//       id="basic-menu"
+//       anchorEl={anchore}
+//       open={open}
+//       onClose={handleClose}
+//       PaperProps={{
+//         style: { maxHeight: 40 * 8, width: "40ch" },
+//       }}
+//     >
 
-const BasicMenu = ({ anchore, handleClose, open, menuItems }) => {
-  console.log(`anchor in basic menu is ${anchore}`);
-  return (
-    <Menu
-      id="basic-menu"
-      anchorEl={anchore}
-      open={open}
-      onClose={handleClose}
-      PaperProps={{
-        style: { maxHeight: 40 * 4, width: "40ch" },
-      }}
-    >
-      {menuItems.map((item) => (
-        <Box bgcolor={item.status === "Unseen" ? "#328da8" : "#69a832"}>
-          <MenuItem onClick={handleClose}>{item.label}</MenuItem>
-        </Box>
-      ))}
-    </Menu>
-  );
-};
+//       {listnf.map((item) => (
+//         <Box bgcolor={bgcolor}>
+//           <MenuItem onClick={clicky}>{item.NotificationContent}</MenuItem>
+//           {/* {
+//             item.label
+//           } */}
+//         </Box>
+//       ))}
 
-export default BasicMenu;
+//     </Menu>
+//   );
+// };
+
+// export default BasicMenu;
--- src/home/frontend/components/notification/NotificationBell.js
+++ src/home/frontend/components/notification/NotificationBell.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Badge from "@mui/material/Badge";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
 import Tooltip from "@mui/material/Tooltip";
 import BasicMenu from "../menu/BasicMenu";
 import { styled } from "@mui/system";
-
+import { listNotifications } from '../../../../gqlFunctions/NotifTable'
+import { API } from 'aws-amplify';
+import * as mutations from '../../../../graphql/mutations';
+import * as queries from '../../../../graphql/queries';
 const notifications = [
   {
     id: 0,
@@ -90,9 +96,34 @@ const notifications = [
 ];
 
 const NotificationBell = ({ iconColor }) => {
+  const [status, setStatus] = useState("Unseen")
+  const [bgcolor, setBgcolor] = useState("#69a832")
+  const [listnf, setListnf] = useState([]);
+  function clicky() {
+    console.log("yes")
+    setBgcolor("white")
+    setStatus("seen")
+  }
+  useEffect(() => {
+    const listNotifications = async () => {
+      try {
+        const listNotifData = await API.graphql({ query: queries.listUserNotifications });
+        console.log(listNotifData.data.listUserNotifications.items);
+        setListnf(listNotifData.data.listUserNotifications.items)
+        console.log(listnf)
+        console.log(listnf.length)
+      } catch (error) {
+        console.log("Error in listing", error)
+        throw new Error(error)
+      }
+    }
+    listNotifications()
+  }, [])
+
+
   const [open, setOpen] = React.useState(false);
   const [anchore, setAnchore] = React.useState(null);
-  const newNotifications = `You have ${notifications.length} new notifications!`;
+  const newNotifications = `You have ${listnf.length} new notifications!`;
   const noNotifications = "No new notifications";
   const handleOpen = (event) => {
     setAnchore(event.currentTarget);
@@ -114,17 +145,31 @@ const NotificationBell = ({ iconColor }) => {
           onClick={notifications.length ? handleOpen : null}
           anchorEl={anchore}
         >
-          <Badge badgeContent={notifications.length} color="error">
+          <Badge badgeContent={listnf.length} color="error">
             <NotificationsIcon />
           </Badge>
         </IconButton>
       </Tooltip>
-      <BasicMenu
+      <Menu
+        id="basic-menu"
+        anchorEl={anchore}
         open={open}
-        anchore={anchore}
-        handleClose={handleClose}
-        menuItems={notifications}
-      />
+        onClose={handleClose}
+        PaperProps={{
+          style: { maxHeight: 40 * 8, width: "40ch" },
+        }}
+      >
+
+        {listnf.map((item) => (
+          <Box bgcolor={bgcolor}>
+            <MenuItem onClick={clicky}>{item.NotificationContent}</MenuItem>
+            {/* {
+            item.label
+          } */}
+          </Box>
+        ))}
+
+      </Menu>
     </div>
   );
 };
